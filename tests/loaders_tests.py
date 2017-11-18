12a660b344bce5016f3bc10a0adfbedc35389a3f
Bryan Shelton <bryan@rover.com>
2017-11-17 16:35:03 -0800

    [snagsby-get] Add snagsby.get that returns an object

--- tests/loaders_tests.py
+++ tests/loaders_tests.py
@@ -27,6 +27,26 @@ class SnagsbyLoadTests(TestCase):
         self.assertEqual(out['TEST'], 'config')
 
 
+class SnagsbyGetTests(TestCase):
+    @patch('snagsby.sanitize')
+    def test_get_noop(self, mock):
+        snagsby.get()
+        self.assertFalse(mock.call_count)
+
+    @patch.object(snagsby.sources.S3Source, 'get_raw_data')
+    def test_get_integration(self, mock):
+        mock.return_value = {
+            'test': 'config',
+        }
+        out = snagsby.get(
+            source='s3://dummy/config.json',
+        )
+        self.assertEqual(out['TEST'], 'config')
+
+    def test_get_defaults_to_empty_obj(self):
+        self.assertEqual(snagsby.get(), {})
+
+
 class SnagsbyLoadObjectTests(TestCase):
     def test_load_object_sanitizes(self):
         obj = {
